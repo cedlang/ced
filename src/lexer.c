4bb62935ead861794ce25d57210abfe2ec033da3
dacousb <usbdaco@protonmail.com>
2022-04-16 13:57:50 +0200

    fix integer token in lexer_next_tok
    also, use clang-format

--- src/lexer.c
+++ src/lexer.c
@@ -37,43 +37,44 @@ str_t tok_type_to_str(toktype_t type) { return to_str(toktype_str[type]); }
 
 static int is_ident(char c) { return isalnum(c) || c == '_'; }
 
-static char cur(lexer_state_t* state) {
-    return state->src.content[state->pos];
-}
+static char cur(lexer_state_t *state) { return state->src.content[state->pos]; }
 
-static str_t read_char(lexer_state_t* state) {
+static str_t read_char(lexer_state_t *state) {
     state->pos++;
     return slice_str(state->src, state->pos - 1, state->pos);
 }
 
-static str_t read_integer(lexer_state_t* state) {
+static str_t read_integer(lexer_state_t *state) {
     size_t start = state->pos;
-    while (isdigit(cur(state))) state->pos++;
+    while (isdigit(cur(state)))
+        state->pos++;
 
     return slice_str(state->src, start, state->pos);
 }
 
-static str_t read_identifier(lexer_state_t* state) {
+static str_t read_identifier(lexer_state_t *state) {
     size_t start = state->pos;
-    while (is_ident(cur(state))) state->pos++;
+    while (is_ident(cur(state)))
+        state->pos++;
 
     return slice_str(state->src, start, state->pos);
 }
 
-tok_t lexer_next_tok(lexer_state_t* state) {
-    while (isspace(cur(state))) state->pos++;
+tok_t lexer_next_tok(lexer_state_t *state) {
+    while (isspace(cur(state)))
+        state->pos++;
 
     tok_t tok;
 
     if (state->pos >= state->src.len) {
         tok.type = TOK_EOF;
         tok.literal = to_str("EOF");
-    } else if (is_ident(cur(state))) {
-        tok.type = TOK_IDENTIFIER;
-        tok.literal = read_identifier(state);
     } else if (isdigit(cur(state))) {
         tok.type = TOK_INTEGER;
         tok.literal = read_integer(state);
+    } else if (is_ident(cur(state))) {
+        tok.type = TOK_IDENTIFIER;
+        tok.literal = read_identifier(state);
     } else {
         switch (cur(state)) {
         case ';':
