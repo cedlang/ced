3b92597b2666da3e10d7dfb1ff7a65c196ef798d
NineSilver <60185955+NineSilver@users.noreply.github.com>
2022-04-20 19:14:46 +0200

    Made build.sh run tests

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 rm -f ced
 gcc $(find src -name "*.c") -O3 -s -Wall -Wextra -Werror -std=c99 -o ced
 
-if [ "$1" == "run" ]
-then
-    ./ced $2
-fi
+case "$1" in
+    "run")
+        ./ced $2
+        ;;
+
+    "test")
+        pushd test
+            ./test.sh
+        popd
+        ;;
+esac
